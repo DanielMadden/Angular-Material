a0a46dce20840ace838224ed62b79f15e1da06aa
Daniel Madden <danielfpjobs@gmail.com>
2021-05-03 15:33:09 -0600

    routing, flex, contactmanager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
-import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'demo',
+    loadChildren: () => import('./demo/demo.module').then((m) => m.DemoModule),
+  },
+  { path: '**', redirectTo: 'demo' },
+];
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent],
